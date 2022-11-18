d5f424c470a3f3ea6c4c40fb69690aa1620d779e
WynstelleID <setiyawandandi@gmail.com>
2022-11-18 16:45:32 +0700

    Wynstelle display simple data with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+const user = {
+  name: "Dandi Setiyawan",
+  location: "South Tangerang",
+  foodType: "Ayam geprek",
+  age: 24,
+  likes: "Coding when u fell asleep",
+  twitterUserName: "dandisetiyawan_",
+  avatar:
+    "https://spesialis1.orthopaedi.fk.unair.ac.id/wp-content/uploads/2021/02/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg",
+};
 
 function App() {
+  const url = `https://twitter.com/${user.twitterUserName}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="user-deets">
+        <img src={user.avatar} alt={user.name} />
+        <h3>{user.name}</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Location</strong>
+          {user.location}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <strong>Eats</strong>
+          {user.foodType}
+        </p>
+        <p>
+          <strong>Age</strong>
+          {user.age}
+        </p>
+        <p>
+          <strong>Likes</strong>
+          {user.likes}
+        </p>
+        <p>
+          <strong>Twitter</strong>
+          <a href={url}>@{user.twitterUserName}</a>
+        </p>
+      </div>
     </div>
   );
 }
